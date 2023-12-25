8eea4a6c72f1d5d3efabe327d5fbbf4efd4613e0
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2023-12-25 12:55:04 -0300

    feat-layout: Feat - Create 'DefaultLayout' component template

--- src/app/core/layouts/default-layout/default-layout.component.ts
+++ src/app/core/layouts/default-layout/default-layout.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+
 @Component({
   selector: 'layout-default',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './default-layout.component.html',
   styleUrl: './default-layout.component.scss'
 })
