22fc856bc791b248a7ffdc03dbdf7f0aef20ffa0
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2023-12-25 12:41:46 -0300

    feat-layou: Feat - Create 'Navbar' component attributes

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { NavbarLink } from './navbar.interface';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  navbarLinks: NavbarLink[] = [
+    {
+      label: 'Início',
+      path: ''
+    },
+    {
+      label: 'Lista',
+      path: 'lista'
+    },
+    {
+      label: 'Gêneros',
+      path: 'generos'
+    },
+    {
+      label: 'Novos episódios',
+      path: 'novos-episodios'
+    }
+  ]
 }
